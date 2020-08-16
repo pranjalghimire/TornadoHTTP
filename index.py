3a525b51231ecb0f7fdc8d9e2b002dab3e830c44
[Pranjal Ghimire] <[ghimirepranjal@protonmail.com]>
2020-08-16 14:49:28 -0500

    finished http protocols

---
+++ index.py
@@ -0,0 +1,32 @@
+import tornado.web 
+import tornado.ioloop
+
+class basicRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello,World")
+
+class staticRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+class queryStringRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        n=int(self.get_argument("n"))
+        r="odd" if n%2 else "even"
+        self.write("The number "+str(n)+" is "+r)
+
+class resourceRequestHandler(tornado.web.RequestHandler):
+    def get(self,id):
+        self.write("Querying tweet with id "+id)
+
+if __name__ == "__main__":
+    app=tornado.web.Application([
+        (r"/",basicRequestHandler),
+        (r"/blog",staticRequestHandler),
+        (r"/isEven",queryStringRequestHandler),
+        (r"/tweet/([0-9]+)",resourceRequestHandler)
+    ])
+
+    app.listen(8881)
+    print("Im listening on port 8881")
+    tornado.ioloop.IOLoop.current().start()

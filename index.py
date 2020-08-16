93e53a19993c36c3a48c7fc9f151bdc31dca9bb1
[Pranjal Ghimire] <[ghimirepranjal@protonmail.com]>
2020-08-16 15:23:19 -0500

    Commented about stop execution

--- index.py
+++ index.py
@@ -1,3 +1,8 @@
+#If the tornado is still listening to port,stop the execution by following commands
+#ps -a
+#kill -9 PID
+#the pid is provided in previous command.Type only the pid where the command is for python3
+
 import tornado.web 
 import tornado.ioloop
 
@@ -30,3 +35,4 @@ if __name__ == "__main__":
     app.listen(8881)
     print("Im listening on port 8881")
     tornado.ioloop.IOLoop.current().start()
+
